317e7bb4d0be400d512baf3d79ad2ccff4fae8ef
mkmelhundley-code <mkmelhundley@gmail.com>
2025-10-15 13:02:25 -0700

    Improved UX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -18,9 +17,9 @@ function generatePoem(event) {
     "You are an expert in lyric poetry and love writing short poems. Your mission is to write a 4 line poem in basic HTML format using a <p> element and separate each line with a <br />. Do not include a title for the poem. Make sure to follow the user's instructions. Sign the poem with 'SheCodes AI' in a <strong> element at the end of the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem for you about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
